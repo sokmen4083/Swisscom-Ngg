c7f65095d7c9b15f3d656a6e37c276820421a1d6
sokmen4083 <sokmen4083@gmail.com>
2021-05-05 12:59:14 +0200

    style has done

---
+++ app.js
@@ -0,0 +1,20 @@
+function who(){
+    swal('Now you will know me a little')
+    document.getElementById('wrapper').style.display = 'block'
+}
+function about(){
+    swal('Now you will see which schools I attend')
+    document.getElementById('about').style.display = 'block'
+}
+function work(){
+    swal('Now you will see where I work which job')
+    document.getElementById('work').style.display = 'block'
+}
+function hobby(){
+    swal('Now you will see my hobbies')
+    document.getElementById('hobby').style.display = 'block'
+}
+function video(){
+    swal('Now you will see a video about Switzerland')
+    document.getElementById('video').style.display = 'block'
+}
